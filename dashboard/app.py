6514f02d410b4eaa41db936c5dfc1c5112ff9b62
Ed Singleton <singletoned@gmail.com>
2023-02-28 17:47:48 +0000

    Add sample chart

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,41 @@
-import streamlit as st
+import numpy as np  # np mean, np random
+import pandas as pd  # read csv, df manipulation
+import plotly.express as px  # interactive charts
+import streamlit as st  # web development
 
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+year_col, continent_col, log_x_col = st.columns(3)
+
+with year_col:
+    year_choice = st.slider(
+        "What year would you like to examine?",
+        min_value=1952,
+        max_value=2007,
+        step=5,
+        value=2007,
+    )
+with continent_col:
+    continent_choice = st.selectbox(
+        "What continent would you like to look at?",
+        ("All", "Asia", "Europe", "Africa", "Americas", "Oceania"),
+    )
+with log_x_col:
+    log_x_choice = st.checkbox("Log X Axis?")
+
+
+df = px.data.gapminder()
+filtered_df = df[(df.year == year_choice)]
+if continent_choice != "All":
+    filtered_df = filtered_df[filtered_df.continent == continent_choice]
+
+fig = px.scatter(
+    filtered_df,
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=log_x_choice,
+    size_max=60,
+)
+fig.update_layout(title="GDP per Capita vs. Life Expectancy")
+st.plotly_chart(fig, use_container_width=True)
